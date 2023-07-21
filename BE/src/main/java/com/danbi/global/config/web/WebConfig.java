223284482291d58c65093d2803a860fff164a87e
DESKTOP-DOGVPUB\SSAFY <hm03048@naver.com>
2023-07-21 10:48:02 +0900

    feat: WebConfig 설정

---
+++ BE/src/main/java/com/danbi/global/config/web/WebConfig.java
@@ -0,0 +1,27 @@
+package com.danbi.global.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**") // 어떤 api 경로에 매핑할지
+                .allowedOrigins("http://localhost:8082") // cors 허용할 경로
+//                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                .maxAge(3600); // preflight
+    }
+}
