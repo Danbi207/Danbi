44d25b5dd910a9d5e2414881f43f258d6d2a2dd4
강민석 <wwwttt123@naver.com>
2023-07-21 17:04:29 +0900

    fix : Footer 파일 수정

--- FE/danbi/src/Components/Common/Footer/Fppter.jsx
+++ FE/danbi/src/Components/Common/Footer/Fppter.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
   return (
     <FooterWrap>
-      
-
-    </FooterWrap>
+      <Link to=''><img src="assets/friend-black1" alt="" /></Link>
+      <div>안녕</div>
+    </FooterWrap> 
   )
 }
 const FooterWrap = styled.div`
@@ -14,6 +16,8 @@ const FooterWrap = styled.div`
     position: fixed;
     bottom: 0;
     background-color: black;
-    
 `
+
+
+
 export default Footer
