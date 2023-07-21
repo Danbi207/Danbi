a2a6790b0530a2ca87397fb7670ce26b3094a1aa
한승현 <vkflek1232@naver.com>
2023-07-21 15:31:03 +0900

    feat : react-cookie 적용

--- FE/danbi/src/App.js
+++ FE/danbi/src/App.js
@@ -9,29 +9,32 @@ import routes from "./router";
 import store from "./store";
 import {dark,light} from "./style/theme.js";
 import { ThemeProvider } from '@tanstack/react-query-devtools/build/lib/theme';
+import { CookiesProvider } from 'react-cookie';
 const queryClient = new QueryClient();
 function App() {
   const theme = themeMode === 'light' ? light : dark;
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-        <div className="App">
-          <BrowserRouter>
-            <Routes>
-              {routes.map((e) => {
-                return (
-                  <Route
-                    key={e.path}
-                    path={e.path}
-                    element={<e.compoonent />}
-                  />
-                );
-              })}
-            </Routes>
-          </BrowserRouter>
-        </div>
-        </ThemeProvider>
+        <CookiesProvider>
+          <ThemeProvider theme={theme}>
+            <div className="App">
+              <BrowserRouter>
+                <Routes>
+                  {routes.map((e) => {
+                    return (
+                      <Route
+                        key={e.path}
+                        path={e.path}
+                        element={<e.compoonent />}
+                      />
+                    );
+                  })}
+                </Routes>
+              </BrowserRouter>
+            </div>
+          </ThemeProvider>
+        </CookiesProvider>
       </Provider>
     </QueryClientProvider>
   );
