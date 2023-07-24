cfc0436d780408ceaf08ab5666277f6f25c448c0
김윤욱 <hm03048@naver.com>
2023-07-24 16:24:37 +0900

    feat: kakao access-token으로 회원 정보 가져오는 기능

---
+++ BE/src/main/java/com/danbi/web/client/KakaoAccessTokenClient.java
@@ -0,0 +1,21 @@
+package com.danbi.web.client;
+
+import com.danbi.api.login.dto.OauthLoginDto;
+import com.danbi.web.dto.KakaoAccessTokenRequest;
+import feign.HeaderMap;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.Map;
+
+@FeignClient(url = "http://192.168.100.83:8080/api/v1", name = "kakaoAccessTokenClient")
+public interface KakaoAccessTokenClient {
+
+    @PostMapping(value = "/oauth/login", consumes = "application/json")
+    OauthLoginDto.Response requestKakaoUserInfo(@RequestHeader("Authorization") String authorization,
+                                                @RequestBody KakaoAccessTokenRequest request
+    );
+}
