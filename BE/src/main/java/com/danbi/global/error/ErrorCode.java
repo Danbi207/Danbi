e2625acc712c5b762d786e1642886bc1663a5c4e
김윤욱 <hm03048@naver.com>
2023-07-24 14:00:48 +0900

    feat: 회원 가입, 조회 ErrorCode

--- BE/src/main/java/com/danbi/global/error/ErrorCode.java
+++ BE/src/main/java/com/danbi/global/error/ErrorCode.java
@@ -11,6 +11,11 @@ public enum ErrorCode {
     NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED, "A-002", "해당 토큰은 유효한 토큰이 아닙니다."),
     NOT_EXISTS_AUTHORIZATION(HttpStatus.UNAUTHORIZED, "A-003", "Authorization Header가 빈값입니다."),
     NOT_VALID_BEARER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "A-004", "인증 타입이 Bearer 타입이 아닙니다."),
+
+    // 회원
+    INVALID_MEMBER_TYPE(HttpStatus.BAD_REQUEST, "M-001", "잘못된 회원 타입 입니다.(memberType : KAKAO)"),
+    ALREADY_REGISTERED_MEMBER(HttpStatus.BAD_REQUEST, "M-002", "이미 가입된 회원 입니다."),
+    MEMBER_NOT_EXISTS(HttpStatus.BAD_REQUEST, "M-003", "해당 회원은 존재하지 않습니다."),
     ;
 
     ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
