e6b9a3cbfe24c663fe29222d75ec77aa41a65012
한승현 <vkflek1232@naver.com>
2023-07-24 13:31:25 +0900

    feat : 로그인기능 완성

--- FE/danbi/src/Components/Login/oauth/KakaoOauth.jsx
+++ FE/danbi/src/Components/Login/oauth/KakaoOauth.jsx
@@ -5,15 +5,14 @@ const KaKaoOauth = () => {
   useEffect(()=>{
     //인가코드 서버에 전달 및 accesstoken받기
     const code = new URL(window.location.href).searchParams.get("code");
-    alert(process.env.REACT_APP_KAKAO_OUATH__URI);
     axios({
       method : "post",
       url : process.env.REACT_APP_KAKAO_OUATH__URI,
       data : {code}
-    }).then(()=>{
-      alert("성공");
-    }).catch(()=>{
-      alert("실패");
+    }).then(({data})=>{
+      console.log(data);
+    }).catch((err)=>{
+      console.log(err);
     });
   },[])
   return (
