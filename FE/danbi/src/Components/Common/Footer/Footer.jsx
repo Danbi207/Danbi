a0dd7917963faff251a3575c54d7c1f0f2fb8bbe
강민석 <wwwttt123@naver.com>
2023-07-24 16:54:15 +0900

    Feat : Footer 화이트, 블랙 이미지 연결

--- FE/danbi/src/Components/Common/Footer/Footer.jsx
+++ FE/danbi/src/Components/Common/Footer/Footer.jsx
@@ -1,35 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
   const navigate = useNavigate();
 
-  return (
-    <FooterWrap>
-      {/* <Link to='/'><img src='assets/home-filled-black.svg' alt='기다'></img></Link>
-      <Link to='/'><img src='assets/home-filled-black.svg' alt='기다'></img></Link>
-      <Link to='/'><img src='assets/home-filled-black.svg' alt='기다'></img></Link> */}
-      
-      <img onClick={()=>{navigate("/") }} src="assets/Friends-black.svg" alt="안나옴" />
-      <img onClick={()=>{navigate("/") }} src="assets/home-filled-black.svg" alt="안나옴" />
-      <img onClick={()=>{navigate("/") }} src="assets/profile-black.svg" alt="안나옴" />
-      
+  let [FriendsFlag, setFriendsFlag] = useState(true);
+  let [HomeFlag, setHomeFlag] = useState(true);
+  let [ProfileFlag, setProfileFlag] = useState(false);
 
+  useEffect(()=>{
+    console.log(FriendsFlag);
+  },[FriendsFlag])
+
+  return (
+    <FooterWrap>      
+      <FriendsImg onClick={()=>{setFriendsFlag(true); setHomeFlag(false); setProfileFlag(false); navigate("/friends");}}></FriendsImg>
+      <Homeimg onClick={()=>{setFriendsFlag(true); setHomeFlag(true); setProfileFlag(false); navigate("/") }}></Homeimg>
+      <ProfileImg onClick={()=>{setFriendsFlag(true); setHomeFlag(false); setProfileFlag(true); navigate("/profile") }}></ProfileImg>
     </FooterWrap> 
   )
 }
 const FooterWrap = styled.div`
     width: 100%;
-    height: 4rem;
+    height: 3.2rem;
     position: fixed;
     bottom: 0;
-    background-color: black;
+    background-color: ${props=>props.theme.colors.bgColor};
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
 `
+
+const Img = styled.div`
+  width : 90%;
+  height : 90%;
+` 
+const FriendsImg = styled(Img).attrs((props) => ({
+  style: {
+    backgroundImage: `url(${
+      props.FriendsFlag
+      ? props.theme.images.friendsFilled // 친구목록 페이지 이동시 이미지 URL
+      : props.theme.images.friends // 그외의 친구목록 이미지 URL
+    })`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: '3rem',
+  },
+}))``;
+
+const Homeimg = styled(Img).attrs()`
+  background-image : url(${props=>props.theme.images.homeFilled});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 2.5rem;
+`
+const ProfileImg = styled(Img).attrs()`
+  background-image : url(${props=>props.theme.images.profile});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 2.2rem;
+`
+
+
+
+
+
+
 
 
 
--- FE/danbi/src/style/theme.js
+++ FE/danbi/src/style/theme.js
@@ -4,16 +4,29 @@ export const dark ={
     bgColor: '#19191B',
   },
   images:{
-    logo : `${process.env.PUBLIC_URL}/assets/Danbi-Logo-black.svg`
+    logo : `${process.env.PUBLIC_URL}/assets/Danbi-Logo-black.svg`,
+    friends : `${process.env.PUBLIC_URL}/assets/Friends-black.svg`,
+    friendsFilled : `${process.env.PUBLIC_URL}/assets/Friends-Filled-black.svg`,
+    home : `${process.env.PUBLIC_URL}/assets/Home-black.svg`,
+    homeFilled : `${process.env.PUBLIC_URL}/assets/Home-Filled-black.svg`,
+    profile : `${process.env.PUBLIC_URL}/assets/Profile-black.svg`,
+    profileFiiled : `${process.env.PUBLIC_URL}/assets/Profile-Filled-black.svg`,
+
   }
-}
+} 
 
 export const light ={
   colors:{
     titleColor : '#19191B',
-    bgColor: '#FFFFFF',
+    bgColor: '#FFFFFF', 
   },
   images:{
-    logo : `${process.env.PUBLIC_URL}/assets/Danbi-Logo-white.svg`
+    logo : `${process.env.PUBLIC_URL}/assets/Danbi-Logo-white.svg`,
+    friends : `${process.env.PUBLIC_URL}/assets/Friends-white.svg`,
+    friendsFille : `${process.env.PUBLIC_URL}/assets/Friends-Filled-white .svg`,
+    home : `${process.env.PUBLIC_URL}/assets/Home-white.svg`,
+    homeFilled : `${process.env.PUBLIC_URL}/assets/Home-Filled-white.svg`,
+    profile : `${process.env.PUBLIC_URL}/assets/Profile-white.svg`,
+    profileFiiled : `${process.env.PUBLIC_URL}/assets/Profile-Filled-white.svg`,
   }
 }
