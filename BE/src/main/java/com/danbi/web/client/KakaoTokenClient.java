f26af7206c320b40413be50ffa785cb81925ff50
김윤욱 <hm03048@naver.com>
2023-07-24 13:30:44 +0900

    feat: Kakao로 인증 토큰 보내는 FeignClient 구현

---
+++ BE/src/main/java/com/danbi/web/client/KakaoTokenClient.java
@@ -0,0 +1,17 @@
+package com.danbi.web.client;
+
+import com.danbi.web.dto.KakaoTokenDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(url = "https://kauth.kakao.com", name = "kakaoTokenClient")
+public interface KakaoTokenClient {
+
+    @PostMapping(value = "/oauth/token", consumes = "application/json")
+    KakaoTokenDto.Response requestKakaoToken(@RequestHeader("Content-Type") String contentType,
+                                             @SpringQueryMap KakaoTokenDto.Request request // query 많을 때 사용
+    );
+
+}
