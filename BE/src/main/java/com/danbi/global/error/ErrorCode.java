fce5fb31ede6c201afebec21a5cd17315b74a4d5
김윤욱 <hm03048@naver.com>
2023-07-24 16:49:21 +0900

    feat: access-token이 아닐 때 ErrorCode

--- BE/src/main/java/com/danbi/global/error/ErrorCode.java
+++ BE/src/main/java/com/danbi/global/error/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     NOT_VALID_BEARER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "A-004", "인증 타입이 Bearer 타입이 아닙니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A-005", "해당 refresh token은 존재하지 않습니다."),
     REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-006", "해당 refresh token은 만료됐습니다."),
+    NOT_ACCESS_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "A-007", "해당 토큰은 ACCESS TOKEN이 아닙니다."),
 
     // 회원
     INVALID_MEMBER_TYPE(HttpStatus.BAD_REQUEST, "M-001", "잘못된 회원 타입 입니다.(memberType : KAKAO)"),
