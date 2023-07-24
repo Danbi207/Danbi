aeef2394780c8f67178661a2e6b1702b46f17c51
김윤욱 <hm03048@naver.com>
2023-07-24 14:12:56 +0900

    feat: OauthLoginService

---
+++ BE/src/main/java/com/danbi/api/login/service/OauthLoginService.java
@@ -0,0 +1,50 @@
+package com.danbi.api.login.service;
+
+import com.danbi.api.login.dto.OauthLoginDto;
+import com.danbi.domain.member.constant.OauthType;
+import com.danbi.domain.member.constant.Role;
+import com.danbi.domain.member.entity.Member;
+import com.danbi.domain.member.service.MemberService;
+import com.danbi.external.oauth.model.OAuthAttributes;
+import com.danbi.external.oauth.service.SocialLoginApiService;
+import com.danbi.external.oauth.service.SocialLoginApiServiceFactory;
+import com.danbi.global.jwt.dto.JwtDto;
+import com.danbi.global.jwt.service.TokenManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
+public class OauthLoginService {
+
+    private final MemberService memberService;
+    private final TokenManager tokenManager;
+
+    public OauthLoginDto.Response oauthLogin(String accessToken, OauthType oauthType) {
+        SocialLoginApiService socialLoginApiService = SocialLoginApiServiceFactory.getSocialLoginApiService(oauthType);
+        OAuthAttributes userInfo = socialLoginApiService.getUserInfo(accessToken);
+        log.info("userInfo : {}",  userInfo);
+
+        JwtDto jwtTokenDto;
+        Optional<Member> optionalMember = memberService.findMemberByEmail(userInfo.getEmail());
+        Member oauthMember;
+        if(optionalMember.isEmpty()) { // 신규 회원 가입
+            oauthMember = userInfo.toMemberEntity(oauthType, Role.ROLE_UNDEFINED);
+            oauthMember = memberService.registerMember(oauthMember);
+        } else { // 기존 회원일 경우
+            oauthMember = optionalMember.get();
+        }
+        // 토큰 생성
+        jwtTokenDto = tokenManager.createJwtTokenDto(oauthMember.getId(), oauthMember.getRole());
+        oauthMember.updateRefreshToken(jwtTokenDto);
+
+        return OauthLoginDto.Response.of(jwtTokenDto);
+    }
+
+}
