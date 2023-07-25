aa416891e73e7379db74f4599a9044a1f403507c
한승현 <vkflek1232@naver.com>
2023-07-25 09:52:20 +0900

    hotfix : redirectUrl 추가

--- FE/danbi/src/Components/Login/oauth/KakaoOauth.jsx
+++ FE/danbi/src/Components/Login/oauth/KakaoOauth.jsx
@@ -5,10 +5,11 @@ const KaKaoOauth = () => {
   useEffect(()=>{
     //인가코드 서버에 전달 및 accesstoken받기
     const code = new URL(window.location.href).searchParams.get("code");
+    const redirectUrl = `http://${window.location.hostname}${process.env.REACT_APP_KAKAO_OAUTH_REDIRECT_URI}`
     axios({
       method : "post",
       url : process.env.REACT_APP_KAKAO_OUATH__URI,
-      data : {code}
+      data : {code,redirectUrl}
     }).then(({data})=>{
       console.log(data);
     }).catch((err)=>{
