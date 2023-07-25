6b20f60a338f840c1232634f53846e2787ab393e
윤태웅 <omygog@naver.com>
2023-07-25 16:06:24 +0900

    style : 프리셋 모달창 디테일 위치 수정

--- FE/danbi/src/Components/Profile/Preset.jsx
+++ FE/danbi/src/Components/Profile/Preset.jsx
@@ -25,13 +25,15 @@ const Preset = () => {
     return(
         <PresetWrap>
             {preset_list.map((value, index) => (
-                <ElementBtn onClick={() => {showDetail(index)}} key={index}>
-                    <PreSetElement>
-                        {value.content ? value.content : `프리셋 ${index + 1}`}
-                    </PreSetElement>
-                </ElementBtn>
+                <Wrap>
+                    <ElementBtn onClick={() => {showDetail(index)}} key={index}>
+                        <PreSetElement>
+                            {value.content ? value.content : `프리셋 ${index + 1}`}
+                        </PreSetElement>
+                    </ElementBtn>
+                    {OpenIndex === index && <PresetDetail key={index} content={preset_list[index].content} showDetail={showDetail} />}
+                </Wrap>
             ))}
-            {(OpenIndex !== -1 && preset_list[OpenIndex].content) && <PresetDetail content={preset_list[OpenIndex].content} showDetail={showDetail} />}
         </PresetWrap>
     )
 }
@@ -41,10 +43,17 @@ const PresetWrap = styled.div`
     height: 1.5rem;
 `
 
+const Wrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: auto;
+    margin-bottom: 0.5rem;
+`
+
 const ElementBtn = styled.button`
     border: 1px solid white;
     border-radius: 5px;
-    margin-bottom: 0.5rem;
+    margin-bottom: 0.25rem;
 `
 
 const PreSetElement = styled.div`
