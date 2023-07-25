afc7b43b1b949d41270960044c5a1c169453ce2f
김윤욱 <hm03048@naver.com>
2023-07-25 15:17:19 +0900

    feat: Profile Service 구현

--- BE/src/main/java/com/danbi/domain/profile/service/ProfileService.java
+++ BE/src/main/java/com/danbi/domain/profile/service/ProfileService.java
@@ -3,9 +3,13 @@ package com.danbi.domain.profile.service;
 import com.danbi.domain.member.entity.Member;
 import com.danbi.domain.profile.entity.Profile;
 import com.danbi.domain.profile.repository.ProfileRepository;
+import com.danbi.global.error.ErrorCode;
+import com.danbi.global.error.exception.ProfileNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,11 +18,21 @@ public class ProfileService {
 
     private final ProfileRepository profileRepository;
 
-    public Profile getProfile(Long profileId) {
-        return profileRepository.findById(profileId).get();
+    public Profile getProfileById(Long profileId) {
+        Optional<Profile> op = profileRepository.findById(profileId);
+        if(op.isEmpty()) {
+            throw new ProfileNotFoundException(ErrorCode.PROFILE_NOT_EXISTS);
+        }
+
+        return op.get();
     }
 
     public Profile getProfileByMember(Member member) { // 아이템 뽑기용
-        return profileRepository.findByMember(member).get();
+        Optional<Profile> op = profileRepository.findByMember(member);
+        if(op.isEmpty()) {
+            throw new ProfileNotFoundException(ErrorCode.PROFILE_NOT_EXISTS);
+        }
+
+        return op.get();
     }
 }
