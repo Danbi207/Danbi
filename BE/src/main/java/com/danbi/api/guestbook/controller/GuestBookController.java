364ffa2d3511b56736ecf71da4a337fdcea07aae
김윤욱 <hm03048@naver.com>
2023-07-25 15:28:09 +0900

    feat: 방명록 조회 swagger

--- BE/src/main/java/com/danbi/api/guestbook/controller/GuestBookController.java
+++ BE/src/main/java/com/danbi/api/guestbook/controller/GuestBookController.java
@@ -2,10 +2,13 @@ package com.danbi.api.guestbook.controller;
 
 import com.danbi.api.guestbook.dto.GuestBookResponseDto;
 import com.danbi.api.guestbook.service.GuestBookProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "GuestBook", description = "방명록")
 @RestController
 @RequestMapping("/api/v1/profile/guestbook")
 @RequiredArgsConstructor
@@ -13,6 +16,7 @@ public class GuestBookController {
 
     private final GuestBookProfileService guestBookProfileService;
 
+    @Operation(summary = "해당 프로필의 방명록 조회 API", description = "프로필의 Id 값으로 해당 방명록 조회")
     @GetMapping("/{profileId}")
     public ResponseEntity<GuestBookResponseDto> getGuestBook(@PathVariable Long profileId) {
         GuestBookResponseDto guestBookResponseDto = guestBookProfileService.getGuestBook(profileId);
--- BE/src/main/java/com/danbi/api/guestbook/dto/GuestBookResponseDto.java
+++ BE/src/main/java/com/danbi/api/guestbook/dto/GuestBookResponseDto.java
@@ -1,5 +1,6 @@
 package com.danbi.api.guestbook.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,29 +8,46 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Schema(description = "방명록 조회 응답 DTO")
 @Setter
 @Getter
 @Builder
 public class GuestBookResponseDto {
 
+    @Schema(description = "방명록 id")
     private Long guest_book_id;
+
+    @Schema(description = "방명록 정보")
     private GuestBookDto guestBookDto;
 
     @Builder
     @Setter
     @Getter
     public static class GuestBookDto {
+
+        @Schema(description = "방명록 안에 있는 댓글 목록")
         private List<CommentDto> commentDtos;
 
         @Getter
         @Setter
         @Builder
         public static class CommentDto {
+            @Schema(description = "댓글 id")
             private Long commentId;
+
+            @Schema(description = "댓글 작성자 이름")
             private String name;
+
+            @Schema(description = "댓글 작성자 profile_url")
             private String profileUrl;
+
+            @Schema(description = "댓글 내용")
             private String content;
+
+            @Schema(description = "댓글 생성 시간")
             private LocalDateTime createdTime;
+
+            @Schema(description = "댓글 수정 시간")
             private LocalDateTime updatedTime;
         }
 
