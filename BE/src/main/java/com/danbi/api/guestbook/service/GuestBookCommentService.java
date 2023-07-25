22a3e6f1949be0774740bbbf5bf5d33060e3da2b
김윤욱 <hm03048@naver.com>
2023-07-25 17:20:02 +0900

    feat: 댓글 저장 기능

---
+++ BE/src/main/java/com/danbi/api/guestbook/service/GuestBookCommentService.java
@@ -0,0 +1,42 @@
+package com.danbi.api.guestbook.service;
+
+import com.danbi.api.guestbook.dto.CommentDto;
+import com.danbi.api.guestbook.dto.GuestBookResponseDto;
+import com.danbi.domain.comment.entity.Comment;
+import com.danbi.domain.comment.service.CommentService;
+import com.danbi.domain.guestbook.entity.GuestBook;
+import com.danbi.domain.guestbook.service.GuestBookService;
+import com.danbi.domain.member.entity.Member;
+import com.danbi.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GuestBookCommentService {
+
+    private final CommentService commentService;
+    private final GuestBookService guestBookService;
+    private final MemberService memberService;
+
+    public CommentDto.Response saveComment(Long guestBookId, CommentDto.Reqeust reqeust,Long memberId) {
+        GuestBook guestBook = guestBookService.getGuestBookById(guestBookId);
+        Member member = memberService.findByMemberId(memberId);
+
+        Comment comment = Comment.builder()
+                .content(reqeust.getContent())
+                .member(member)
+                .guestBook(guestBook)
+                .build();
+
+        Comment savedComment = commentService.saveComment(comment);
+        return CommentDto.Response.builder()
+                .id(savedComment.getId())
+                .content(savedComment.getContent())
+                .name(member.getName())
+                .createTime(savedComment.getCreateTime())
+                .updateTime(savedComment.getUpdateTime())
+                .build();
+    }
+
+}
