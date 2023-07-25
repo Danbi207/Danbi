402196f0290ea4fd42670258ea27b3b24d4d5a58
한승현 <vkflek1232@naver.com>
2023-07-25 12:30:36 +0900

    feat : 카카오 맵 지도

--- FE/danbi/src/Components/Helper/Home/HelpMap/HelpMap.jsx
+++ FE/danbi/src/Components/Helper/Home/HelpMap/HelpMap.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components';
 const HelpMap = () => {
+  const {kakao} = window;
+  const mapRef = useRef();
+  useEffect(()=>{
+    const mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    const map = new kakao.maps.Map(mapRef.current, mapOption); 
+  },[mapRef,kakao])
   return (
-    <div>HelpMap</div>
+    <MapWrap id='map' ref={mapRef}>HelpMap</MapWrap>
   )
 }
-
+const MapWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+`
 export default HelpMap
--- FE/danbi/src/Components/Helper/Home/HelperHome.jsx
+++ FE/danbi/src/Components/Helper/Home/HelperHome.jsx
@@ -20,7 +20,9 @@ const HelperHome = () => {
 const HelperHomeWrap = styled.div`
   display: flex;
   flex-direction: column;
-
+  width: 100%;
+  height: 100%;
+  position: relative;
 `
 
 export default HelperHome
