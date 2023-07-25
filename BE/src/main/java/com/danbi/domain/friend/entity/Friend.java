26eb920540f509e4c530a83b55d1ecb429da477b
jyj1143 <jyj1143@gmail.com>
2023-07-26 05:54:17 +0900

    fix: Firend Entity 삭제유무 속성 추가

--- BE/src/main/java/com/danbi/domain/friend/entity/Friend.java
+++ BE/src/main/java/com/danbi/domain/friend/entity/Friend.java
@@ -1,9 +1,11 @@
 package com.danbi.domain.friend.entity;
 
-import com.danbi.domain.alarm.constant.Type;
 import com.danbi.domain.common.BaseEntity;
+import com.danbi.domain.friend.constant.State;
+import com.danbi.domain.friend.constant.Type;
 import com.danbi.domain.member.entity.Member;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,6 +30,30 @@ public class Friend extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
-    private Type type;
+    private Type type = Type.WAIT;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private State state = State.ACTIVATE;
+
+
+    @Builder
+    public Friend(Member from, Member to, Type type, State state) {
+        this.from = from;
+        this.to = to;
+        this.type = type;
+        this.state = state;
+    }
+
+    public void update(Friend friend) {
+        this.from = friend.getFrom();
+        this.to = friend.getTo();
+        this.type = friend.getType();
+        this.state = friend.getState();
+    }
+
+    public void delete() {
+        this.state = State.DESTROY;
+    }
 
 }
