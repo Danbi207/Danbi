5f2e96429e643d338d1659314366b0e2f9d99a9b
한승현 <vkflek1232@naver.com>
2023-07-26 10:23:33 +0900

    refactor : css style 수정

--- FE/danbi/src/Components/Login/DocSubmit.jsx
+++ FE/danbi/src/Components/Login/DocSubmit.jsx
@@ -3,42 +3,38 @@ import styled from 'styled-components'
 import Uploader from './Uploader';
 
 const DocSubmit = () => {
-  
-
   return (
-      <>
-        <SubmitWrap>
-          <Question>서류를 제출해주세요</Question>
-          <Uploader></Uploader>
-          <NextButton>다음</NextButton>
-        </SubmitWrap>
-      </>
+    <SubmitWrap>
+      <Question>서류를 제출해주세요</Question>
+      <Uploader></Uploader>
+      <NextButton>다음</NextButton>
+    </SubmitWrap>
   );
 }
 
 const SubmitWrap = styled.div`
-    width: 100%;
-    height: 1;
-    background-color: ${props=>props.theme.colors.bgColor};
-    display: flex;
-    flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: ${props=>props.theme.colors.bgColor};
 `
 const Question = styled.div`
-    font-size: 1.6rem;
-    height: 1rem;
-    text-align: center;
-    margin-bottom: 2rem;
-    margin-top: 3.5rem;
+  width: 100%;
+  height: 5rem;
+  line-height: 5rem;
+  font-size: 1.5rem;
+  text-align: center;
 `
 
 const NextButton  = styled.button`
-    width: 20rem;
-    height: 3.1rem;
-    border-radius: 10px;
-    background-color: #6161FF;
-    color: #fff;
-    font-size : 2rem;
-    margin: 5rem auto 1rem auto ;
+  position: absolute;
+  left: calc((100% - 20rem)/2);
+  bottom: 1rem;
+  width: 20rem;
+  height: 3rem;
+  border-radius: 0.75rem;
+  background-color: #6161FF;
+  color: #fff;
+  font-size : 2rem;
 `
 
 export default DocSubmit;
