0b0214fabf3144bfb55d9d49c3ffa340848cd11f
강민석 <wwwttt123@naver.com>
2023-07-26 17:36:58 +0900

    Feat : 캘린더 관련 라이브러리 설치

---
+++ FE/danbi/src/Components/IP/Calender.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import styled from 'sty'
+
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // css import
+// import moment from 'moment';
+
+
+function Calender() {
+  const [value, onChange] = useState(new Date());
+  
+
+  return (
+    <CalenderWrap>
+      <Calendar onChange={onChange} value={value} />
+    </CalenderWrap>
+  );
+}
+
+const CalenderWrap = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+
+export default Calender;
