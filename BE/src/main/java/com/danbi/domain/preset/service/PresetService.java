a7fc9f4026e6b26831e77e35f78c202c9c6781f6
김윤욱 <hm03048@naver.com>
2023-07-27 15:56:52 +0900

    feat: 프리셋 데이터 수정 기능

--- BE/src/main/java/com/danbi/domain/preset/service/PresetService.java
+++ BE/src/main/java/com/danbi/domain/preset/service/PresetService.java
@@ -1,11 +1,16 @@
 package com.danbi.domain.preset.service;
 
+import com.danbi.domain.preset.dto.PresetDto;
 import com.danbi.domain.preset.entity.Preset;
 import com.danbi.domain.preset.repository.PresetRepository;
+import com.danbi.global.error.ErrorCode;
+import com.danbi.global.error.exception.notfound.PresetNotFoundException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -18,6 +23,24 @@ public class PresetService {
     @Transactional
     public Preset savePreset(Preset preset) {
         return presetRepository.save(preset);
+    }
+
+    @Transactional
+    public Preset update(PresetDto presetDto, Preset preset) {
+        preset.updateTitle(presetDto.getTitle());
+        preset.updateContent(preset.getContent());
+        preset.updateSequence(presetDto.getSequence());
+
+        return preset;
+    }
+
+    public Preset findById(Long id) {
+        Optional<Preset> op = presetRepository.findById(id);
+        if(op.isEmpty()) {
+            throw new PresetNotFoundException(ErrorCode.PRESET_NOT_EXISTS);
+        }
+
+        return op.get();
     }
 
 }
