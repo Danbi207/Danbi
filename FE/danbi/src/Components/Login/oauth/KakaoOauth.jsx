3c3d0411e23028bade6caa2e9995a167951184a6
한승현 <vkflek1232@naver.com>
2023-07-28 14:40:47 +0900

    refactor : 토큰 test용 코드 추가

--- FE/danbi/src/Components/Login/oauth/KakaoOauth.jsx
+++ FE/danbi/src/Components/Login/oauth/KakaoOauth.jsx
@@ -17,6 +17,7 @@ const KaKaoOauth = () => {
       url : "http://192.168.100.83:8080/api/v1/oauth/kakao/callback",//process.env.REACT_APP_KAKAO_OUATH__URI,
       data : {code,redirectUrl}
     }).then(({data})=>{
+      console.log(data);
       if(data.role === "ROLE_IP_WAIT"){//DO : 서류대기중이면 로그인화면으로 Redirect
         alert("가입승인 대기중입니다!");
         navigate("/", { replace: true });
