aa300da0a864942e3a9d1cfcaa8deb75be5de30c
김윤욱 <hm03048@naver.com>
2023-07-31 17:28:11 +0900

    feat: 배포 준비

--- BE/src/main/java/com/danbi/DanbiApplication.java
+++ BE/src/main/java/com/danbi/DanbiApplication.java
@@ -2,8 +2,10 @@ package com.danbi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/property.properties")
 public class DanbiApplication {
 
 	public static void main(String[] args) {
