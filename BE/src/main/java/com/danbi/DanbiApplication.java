e64b64df71512aea68d33a6a185730b6c53f4abb
김윤욱 <hm03048@naver.com>
2023-08-01 13:40:55 +0900

    dockerdocker

--- BE/src/main/java/com/danbi/DanbiApplication.java
+++ BE/src/main/java/com/danbi/DanbiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:/property.properties")
 public class DanbiApplication {
 
 	public static void main(String[] args) {
