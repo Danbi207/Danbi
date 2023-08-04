e830dbc1e974736ae2326caed6cabc08621b8dad
김윤욱 <hm03048@naver.com>
2023-08-04 14:12:14 +0900

    feat: MemberFileService

---
+++ BE/src/main/java/com/danbi/domain/member/service/MemberFileService.java
@@ -0,0 +1,28 @@
+package com.danbi.domain.member.service;
+
+import com.danbi.domain.member.entity.Member;
+import com.danbi.domain.member.entity.MemberFile;
+import com.danbi.domain.member.repository.MemberFileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberFileService {
+
+    private final MemberFileRepository memberFileRepository;
+
+    @Transactional
+    public MemberFile saveIPCertificationFile(Member member, String fileName, String url) {
+        MemberFile memberFile = MemberFile.builder()
+                .originName(fileName)
+                .url(url)
+                .member(member)
+                .build();
+
+        return memberFileRepository.save(memberFile);
+    }
+
+}
