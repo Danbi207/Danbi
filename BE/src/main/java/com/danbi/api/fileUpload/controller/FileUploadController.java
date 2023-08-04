d85bc30b375fe7c7314d65bdadf529c058e07fa7
김윤욱 <hm03048@naver.com>
2023-08-04 14:14:34 +0900

    feat: 파일 업로드  controller 구현

--- BE/src/main/java/com/danbi/api/fileUpload/controller/FileUploadController.java
+++ BE/src/main/java/com/danbi/api/fileUpload/controller/FileUploadController.java
@@ -2,13 +2,16 @@ package com.danbi.api.fileUpload.controller;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.danbi.api.ApiResponse;
+import com.danbi.api.fileUpload.dto.FileUploadResponseDto;
+import com.danbi.api.fileUpload.service.FileUploadService;
+import com.danbi.global.resolver.MemberInfo;
+import com.danbi.global.resolver.MemberInfoDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/ip")
@@ -16,14 +19,17 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileUploadController {
 
     private final AmazonS3Client amazonS3Client;
+    private final FileUploadService fileUploadService;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
     @PostMapping("/submit/certification")
-    public ApiResponse<String> uploadFile(@RequestParam("file") MultipartFile[] uploadFiles) {
+    public ApiResponse<List<FileUploadResponseDto>> uploadFile(@RequestParam("category") String category,
+                                          @RequestPart("file") List<MultipartFile> uploadFiles,
+                                          @MemberInfo MemberInfoDto memberInfoDto) {
 
-
-        return ApiResponse.ok("ok");
+        List<FileUploadResponseDto> response = fileUploadService.uploadIPFiles(category, uploadFiles, memberInfoDto.getMemberId());
+        return ApiResponse.ok(response);
     }
 }
