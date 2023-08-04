216ca7be04525e1c7b08684ddd0b17d2c90516eb
김윤욱 <hm03048@naver.com>
2023-08-04 14:35:42 +0900

    feat: ip 인증 서류 제출 url 수정

--- BE/src/main/java/com/danbi/api/fileUpload/controller/FileUploadController.java
+++ BE/src/main/java/com/danbi/api/fileUpload/controller/FileUploadController.java
@@ -6,6 +6,7 @@ import com.danbi.api.fileUpload.dto.FileUploadResponseDto;
 import com.danbi.api.fileUpload.service.FileUploadService;
 import com.danbi.global.resolver.MemberInfo;
 import com.danbi.global.resolver.MemberInfoDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/ip")
+@RequestMapping("api/v1/submit")
 @RequiredArgsConstructor
 public class FileUploadController {
 
@@ -24,12 +25,12 @@ public class FileUploadController {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    @PostMapping("/submit/certification")
-    public ApiResponse<List<FileUploadResponseDto>> uploadFile(@RequestParam("category") String category,
-                                          @RequestPart("file") List<MultipartFile> uploadFiles,
-                                          @MemberInfo MemberInfoDto memberInfoDto) {
+    @Operation(summary = "IP 인증 서류 제출 API", description = "IP 인증 서류들 제출")
+    @PostMapping("/ip/certification")
+    public ApiResponse<List<FileUploadResponseDto>> uploadIPFile(@RequestPart("file") List<MultipartFile> uploadFiles,
+                                                               @MemberInfo MemberInfoDto memberInfoDto) {
 
-        List<FileUploadResponseDto> response = fileUploadService.uploadIPFiles(category, uploadFiles, memberInfoDto.getMemberId());
+        List<FileUploadResponseDto> response = fileUploadService.uploadIPFiles("ip", uploadFiles, memberInfoDto.getMemberId());
         return ApiResponse.ok(response);
     }
 }
