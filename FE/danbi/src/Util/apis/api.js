c22717d6919866d7d353c418a90d5441a9827e72
한승현 <vkflek1232@naver.com>
2023-08-05 11:33:27 +0900

    hotfix : token 재발행 url 수정

--- FE/danbi/src/Util/apis/api.js
+++ FE/danbi/src/Util/apis/api.js
@@ -27,7 +27,7 @@ export const reissueAccessToken = ()=>{
 
   axios({
     method:"get",
-    url:"/api/v1/access-token",
+    url:"/api/v1/access-token/issue",
     headers:{"Authentication" : `Bearer ${refreshToken}`}
   }).then(({data})=>{
     token.setAccessToken(data.accessToken);
