80d5b0d203a879b1575126376b2e459df4ed3b03
한승현 <vkflek1232@naver.com>
2023-08-05 10:53:32 +0900

    feat : api분리 및 accessToken 만료로직

---
+++ FE/danbi/src/Util/apis/api.js
@@ -0,0 +1,146 @@
+import axios from "axios";
+import { Token } from "../private/token";
+import { useNavigate } from "react-router-dom";
+const token = new Token();
+const navigate = useNavigate();
+export const setToken = (payload) => {
+  token.setAccessToken(payload);
+}
+
+export const setTokenExpireTime = (payload) => {
+  token.setAccessTokenExpireTime(payload);
+}
+
+const authInstance = (url,options)=>{
+  axios.create({
+    baseURL : url,
+    headers:{"Authentication" : `Bearer ${token.getAccessToken()}`},
+    ...options,
+  });
+}
+
+export const reissueAccessToken = ()=>{
+  const refreshToken = localStorage.getItem("refreshToken");
+  if(!refreshToken || refreshToken===""){
+    return true;
+  }
+
+  axios({
+    method:"get",
+    headers:{"Authentication" : `Bearer ${refreshToken}`}
+  }).then(({data})=>{
+    token.setAccessToken(data.accessToken);
+    token.setAccessTokenExpireTime(data.accessTokenExpireTime);
+  }).catch(err=>{
+    console.log(err.response);
+    localStorage.removeItem("refreshToken");
+    token.clear();
+    return true;
+  })
+
+  return false;
+}
+
+export const authGet = async (url,options)=>{
+  try{
+    const {data} = await authInstance(url,options).get();
+
+    if(data.code === 500){
+      //DO : 토큰만료시 재발급요청
+      if(reissueAccessToken()){
+        //DO : 리프레쉬토큰도 만료시 홈화면으로 이동
+        navigate("/",{replace:true});
+        return null;
+      }
+
+      return await authGet(url,options);
+    }
+
+    if(data.code === 200){
+      return data.data;
+    }
+  }catch(err){
+    console.log(err.response);
+    return null;
+  }
+}
+
+export const authPost = async (url,options)=>{
+  try{
+    const {data} = await authInstance(url,options).post();
+
+    if(data.code === 500){
+      //DO : 토큰만료시 재발급요청
+      if(reissueAccessToken()){
+        //DO : 리프레쉬토큰도 만료시 홈화면으로 이동
+        navigate("/",{replace:true});
+        return null;
+      }
+
+      return await authPost(url,options);
+    }
+
+    if(data.code === 200){
+      return data.data;
+    }
+  }catch(err){
+    console.log(err.response);
+    return null;
+  }
+}
+
+export const authDelete = async (url,options)=>{
+  try{
+    const {data} = await authInstance(url,options).delete();
+
+    if(data.code === 500){
+      //DO : 토큰만료시 재발급요청
+      if(reissueAccessToken()){
+        //DO : 리프레쉬토큰도 만료시 홈화면으로 이동
+        navigate("/",{replace:true});
+        return null;
+      }
+
+      return await authDelete(url,options);
+    }
+
+    if(data.code === 200){
+      return data.data;
+    }
+  }catch(err){
+    console.log(err.response);
+    return null;
+  }
+}
+
+
+export const authPut = async (url,options)=>{
+  try{
+    const {data} = await authInstance(url,options).put();
+
+    if(data.code === 500){
+      //DO : 토큰만료시 재발급요청
+      if(reissueAccessToken()){
+        //DO : 리프레쉬토큰도 만료시 홈화면으로 이동
+        navigate("/",{replace:true});
+        return null;
+      }
+
+      return await authPut(url,options);
+    }
+
+    if(data.code === 200){
+      return data.data;
+    }
+  }catch(err){
+    console.log(err.response);
+    return null;
+  }
+}
+
+const defaultInstance = (url,options)=>{
+  axios.create({
+    baseURL : url,
+    ...options,
+  });
+}
---
+++ FE/danbi/src/Util/private/token.js
@@ -0,0 +1,23 @@
+export class Token{
+  #accessToken = "";
+  #accessTokenExpireTime = "";
+
+  setAccessToken(payload){
+    this.#accessToken = payload;
+  }
+  getAccessToken(){
+    return this.#accessToken;
+  }
+
+  setAccessTokenExpireTime(payload){
+    this.#accessTokenExpireTime = payload;
+  }
+  getAccessTokenExpireTime(){
+    return this.#accessTokenExpireTime;
+  }
+
+  clear(){
+    this.#accessTokenExpireTime = "";
+    this.#accessToken = "";
+  }
+}
