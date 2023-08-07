a3a942c92c5582671d13fea94c254096f6edefc2
jyj143 <jyj1143@gmail.com>
2023-08-07 17:08:28 +0900

    fix:Frontend file 주석 해제

--- FE/danbi/src/Components/IP/Calender/Calender.jsx
+++ FE/danbi/src/Components/IP/Calender/Calender.jsx
@@ -1,28 +1,28 @@
-// import React, { useState } from 'react';
-// import styled from 'sty'
+import React, { useState } from 'react';
+import styled from 'sty'
 
-// import Calendar from 'react-calendar';
-// import 'react-calendar/dist/Calendar.css'; // css import
-// // import moment from 'moment';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // css import
+// import moment from 'moment';
 
 
-// function Calender() {
-//   const [value, onChange] = useState(new Date());
+function Calender() {
+  const [value, onChange] = useState(new Date());
   
 
-//   return (
-//     <CalenderWrap>
-//       <Calendar onChange={onChange} value={value} />
-//     </CalenderWrap>
-//   );
-// }
+  return (
+    <CalenderWrap>
+      <Calendar onChange={onChange} value={value} />
+    </CalenderWrap>
+  );
+}
 
-// const CalenderWrap = styled.div`
-//     width: 100%;
-//     height: 100%;
-// `
-
-
+const CalenderWrap = styled.div`
+    width: 100%;
+    height: 100%;
+`
 
 
-// export default Calender;
+
+
+export default Calender;
