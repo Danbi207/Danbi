c5192c0a2cccdcaf18f502df3c7ce23c43c38732
강민석 <wwwttt123@naver.com>
2023-08-07 17:32:04 +0900

    hotfix : ouath redirect url 수정

--- FE/danbi/src/Components/User/Login/Oauth/KakaoOauth.jsx
+++ FE/danbi/src/Components/User/Login/Oauth/KakaoOauth.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+  import axios from 'axios';
 import React, { useEffect ,useCallback} from 'react'
 import { useDispatch} from "react-redux";
 import { useNavigate } from 'react-router';
