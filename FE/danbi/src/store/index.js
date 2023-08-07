8ac183ba0e652ec004e2e62b94894cb91ad4eee4
한승현 <vkflek1232@naver.com>
2023-08-07 19:35:03 +0900

    hotfix : api Redux제거

--- FE/danbi/src/store/index.js
+++ FE/danbi/src/store/index.js
@@ -3,21 +3,11 @@ import settingReducer from "./Slice/settingSlice.js"
 import ipReducer from './Slice/ipSlice.js'
 import JandiReducer from './Slice/JandiSlice.js'
 import modalReducer from "./Slice/ModalSlice.js";
-import apiReducer from "./Slice/apiSlice.js"
 export default configureStore({
   reducer : {
     setting:settingReducer,
     ip : ipReducer,
     Jandi: JandiReducer,
     modal : modalReducer,
-    api : apiReducer
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-      // serializableCheck: {
-      // ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      // ovActions: false,
-      // },
-    }),
+  }
 });
