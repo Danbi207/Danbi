53e108107279185458bd430479301b149fdea499
jyj143 <jyj1143@gmail.com>
2023-08-07 17:06:23 +0900

    feat: com.google.firebas Messge 사용

--- BE/src/main/java/com/danbi/domain/fcm/service/FcmService.java
+++ BE/src/main/java/com/danbi/domain/fcm/service/FcmService.java
@@ -12,17 +12,15 @@ import com.danbi.global.jwt.constant.GrantType;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
 
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.WebpushConfig;
-import com.google.firebase.messaging.WebpushNotification;
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -51,15 +49,17 @@ public class FcmService {
         return fcmTokenRepository.getToken(email);
     }
 
-    public void sendMessageTo(NotificationRequest notificationRequest) throws IOException {
+    public void sendMessageTo(NotificationRequest notificationRequest) throws IOException, ExecutionException, InterruptedException {
         if(!hasKey(notificationRequest.getEmail())){
             throw new BusinessException(ErrorCode.NOT_EXIST_FCM_TOKEN);
         }
 
         String targetToken = getToken(notificationRequest.getEmail());
-        String message = makeMessage(targetToken, notificationRequest.getTitle(), notificationRequest.getMessage());
-        fcmClient.sendDataMessage(CONTENT_TYPE,
-                GrantType.BEARER.getType() + " " + getAccessToken(),message);
+        Message message = makeMessage(targetToken, notificationRequest.getTitle(), notificationRequest.getMessage());
+        String response = FirebaseMessaging.getInstance().sendAsync(message).get();
+        log.info("SentMessage : {}", response);
+//        fcmClient.sendDataMessage(CONTENT_TYPE,
+//                GrantType.BEARER.getType() + " " + getAccessToken(),message);
     }
 
     //FCM에 push 요청을 보낼 때 인증을 위해 Header에 포함시킬 AccessToken 생성
@@ -80,13 +80,26 @@ public class FcmService {
     }
 
     //FCM 알림 메시지 생성
-    private String makeMessage(String targetToken, String title, String body) throws JsonProcessingException {
-        FcmMessage.Notification noti = new FcmMessage.Notification(title, body, null);
-        FcmMessage.Message message = new FcmMessage.Message(noti, targetToken);
-        FcmMessage fcmMessage = new FcmMessage(false, message);
+    private Message makeMessage(String targetToken, String title, String body) throws JsonProcessingException {
 
 
-        return objectMapper.writeValueAsString(fcmMessage);
+//        FcmMessage.Notification noti = new FcmMessage.Notification(title, body, null);
+//        FcmMessage.Message message = new FcmMessage.Message(noti, targetToken);
+//        FcmMessage fcmMessage = new FcmMessage(false, message);
+
+        Message message = Message.builder()
+                .setToken(targetToken)
+                .setWebpushConfig(WebpushConfig.builder().putHeader("ttl", "300")
+                        .setNotification(new WebpushNotification(title, body))
+                        .setFcmOptions(WebpushFcmOptions.builder()
+                                .build())
+                        .build()
+                )
+                .build();
+        return message;
     }
+
+
+
 
 }
--- FE/danbi/src/Components/IP/Calender/StartTime.jsx
+++ FE/danbi/src/Components/IP/Calender/StartTime.jsx
@@ -1,88 +1,88 @@
-import React from 'react'
-import { useState } from 'react';
-import styled from 'styled-components'
+// import React from 'react'
+// import { useState } from 'react';
+// import styled from 'styled-components'
 
-import SDatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+// import SDatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
 
-import { getHours, getMinutes } from 'date-fns';
-import setHours from "date-fns/setHours";
-import setMinutes from "date-fns/setMinutes";
-import { ko } from "date-fns/esm/locale";
+// import { getHours, getMinutes } from 'date-fns';
+// import setHours from "date-fns/setHours";
+// import setMinutes from "date-fns/setMinutes";
+// import { ko } from "date-fns/esm/locale";
 
 
-const StartTime = () => {
-    // 시작 시간
-    const [startTime, setStartTime] = useState(null);
-    // 종료 시간
-    const [endTime, setEndTime] = useState(null);
-    // 시작 시간을 선택했는지
-    const [isSelected, setIsSelected] = useState(false);
+// const StartTime = () => {
+//     // 시작 시간
+//     const [startTime, setStartTime] = useState(null);
+//     // 종료 시간
+//     const [endTime, setEndTime] = useState(null);
+//     // 시작 시간을 선택했는지
+//     const [isSelected, setIsSelected] = useState(false);
 
-    // 시작 시간이 선택되면 해당 시간 적용 및 isSelected를 true, endTime을 null로
-    const onSelect = (time) => {
-        setStartTime(time);
-        setIsSelected(true);
-        setEndTime(null);
-    };
+//     // 시작 시간이 선택되면 해당 시간 적용 및 isSelected를 true, endTime을 null로
+//     const onSelect = (time) => {
+//         setStartTime(time);
+//         setIsSelected(true);
+//         setEndTime(null);
+//     };
 
-    // 현재 시간 기준 지나간 시간 선택 불가
-    const filterPassedTime = (time) => {
-        const currentDate = new Date();
-        const selectedDate = new Date(time);
+//     // 현재 시간 기준 지나간 시간 선택 불가
+//     const filterPassedTime = (time) => {
+//         const currentDate = new Date();
+//         const selectedDate = new Date(time);
     
-        return currentDate.getTime() < selectedDate.getTime();
-    };
+//         return currentDate.getTime() < selectedDate.getTime();
+//     };
 
-    return (
-        <>
-            <TimeWrap><SDatePicker
-                selected={startTime}
-                onChange={onSelect}
-                locale={ ko }
-                showTimeSelect
-                showTimeSelectOnly
-                timeIntervals={15}
-                minTime={setHours(setMinutes(new Date(), 0), 6)}
-                maxTime={setHours(setMinutes(new Date(), 0), 22)}
-                timeCaption="Time"
-                dateFormat="aa h:mm 시작"
-                placeholderText="시작 시간"
-                className="mt-4"
-                filterTime={filterPassedTime}
-            /></TimeWrap>   
+//     return (
+//         <>
+//             <TimeWrap><SDatePicker
+//                 selected={startTime}
+//                 onChange={onSelect}
+//                 locale={ ko }
+//                 showTimeSelect
+//                 showTimeSelectOnly
+//                 timeIntervals={15}
+//                 minTime={setHours(setMinutes(new Date(), 0), 6)}
+//                 maxTime={setHours(setMinutes(new Date(), 0), 22)}
+//                 timeCaption="Time"
+//                 dateFormat="aa h:mm 시작"
+//                 placeholderText="시작 시간"
+//                 className="mt-4"
+//                 filterTime={filterPassedTime}
+//             /></TimeWrap>   
 
-            {isSelected ? // 시작 시간을 선택해야 종료 시간 선택 가능
-                <TimeWrap><SDatePicker
-                selected={endTime}
-                onChange={(time) => setEndTime(time)}
-                locale={ ko }
-                showTimeSelect
-                showTimeSelectOnly
-                timeIntervals={15}
-                minTime={startTime}
-                maxTime={setHours(setMinutes(new Date(), getMinutes(startTime)), getHours(startTime)+2)} // 시작 시간부터 2시간
-                excludeTimes={[
-                    // 시작 시간 제외
-                    startTime,
-                ]}
-                timeCaption="Time"
-                dateFormat="aa h:mm 종료"
-                placeholderText="종료 시간"
-                className="mt-3"
-            /></TimeWrap>
+//             {isSelected ? // 시작 시간을 선택해야 종료 시간 선택 가능
+//                 <TimeWrap><SDatePicker
+//                 selected={endTime}
+//                 onChange={(time) => setEndTime(time)}
+//                 locale={ ko }
+//                 showTimeSelect
+//                 showTimeSelectOnly
+//                 timeIntervals={15}
+//                 minTime={startTime}
+//                 maxTime={setHours(setMinutes(new Date(), getMinutes(startTime)), getHours(startTime)+2)} // 시작 시간부터 2시간
+//                 excludeTimes={[
+//                     // 시작 시간 제외
+//                     startTime,
+//                 ]}
+//                 timeCaption="Time"
+//                 dateFormat="aa h:mm 종료"
+//                 placeholderText="종료 시간"
+//                 className="mt-3"
+//             /></TimeWrap>
                 
-                : null 
-            }
-        </>
-    );
-};
+//                 : null 
+//             }
+//         </>
+//     );
+// };
 
-const TimeWrap = styled.div `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    line-height: 2rem;
-`
+// const TimeWrap = styled.div `
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     line-height: 2rem;
+// `
 
-export default StartTime;
+// export default StartTime;
