26050a0d75acca54b6c1acefc3bc3a8d094bea9d
김윤욱 <hm03048@naver.com>
2023-08-08 13:32:57 +0900

    feat: IP 인증 서류 조회 swagger

--- BE/src/main/java/com/danbi/api/admin/controller/AdminIPController.java
+++ BE/src/main/java/com/danbi/api/admin/controller/AdminIPController.java
@@ -5,11 +5,14 @@ import com.danbi.api.admin.dto.IPCertFileResponseDto;
 import com.danbi.api.admin.service.AdminIPService;
 import com.danbi.global.resolver.memberinfo.MemberInfo;
 import com.danbi.global.resolver.memberinfo.MemberInfoDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Admin", description = "관리자")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/admin/ip")
@@ -17,6 +20,7 @@ public class AdminIPController {
 
     private final AdminIPService adminIPService;
 
+    @Operation(summary = "IP 회원 인증 서류 조회 API", description = "IP 회원 인증 서류 조회")
     @GetMapping("/file/{ipId}/{fileId}")
     public ApiResponse<List<IPCertFileResponseDto>> findIPCertFiles(@PathVariable Long ipId,
                                                                     @PathVariable Long fileId,
--- BE/src/main/java/com/danbi/api/admin/dto/IPCertFileResponseDto.java
+++ BE/src/main/java/com/danbi/api/admin/dto/IPCertFileResponseDto.java
@@ -1,8 +1,10 @@
 package com.danbi.api.admin.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
+@Schema(description = "IP 회원 인증 서류 조회 결과")
 @Getter
 @Builder
 public class IPCertFileResponseDto {
