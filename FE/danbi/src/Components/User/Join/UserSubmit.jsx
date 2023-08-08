769f8bc34716b5e916ce73e6a0ba849e51f0d3e6
강민석 <wwwttt123@naver.com>
2023-08-08 13:48:43 +0900

    Feat : userType api 등록

--- FE/danbi/src/Components/User/Join/UserSubmit.jsx
+++ FE/danbi/src/Components/User/Join/UserSubmit.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+import UserType from './UserType';
+import UserFile from './UserFile';
+import { useState } from 'react';
 
 const UserSubmit = () => {
+  const role = localStorage.getItem('role')
+  const [usertype, setUserType] = useState(role);
+ 
   return (
-    <div>UserSubmit</div>
+    <SubmitWrap>
+    {
+      role === 'ROLE_UNDEFINED' ? 
+      <UserType role={role} usertype={usertype} setUserType={setUserType}/> : 
+      <UserFile role={role} usertype={usertype} setUserType={setUserType}/>
+    }
+  </SubmitWrap>
   )
 }
 
-export default UserSubmit
+const SubmitWrap = styled.div`
+  width: 100%;
+  height: 100%;
+` 
+
+export default UserSubmit; 
