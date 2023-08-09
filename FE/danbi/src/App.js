e57d975a5cf0c535cdc3082312b0fd9fef2137d1
한승현 <vkflek1232@naver.com>
2023-08-09 17:26:11 +0900

    feat : 로딩 화면 적용을 위한 axios interceptor 적용

--- FE/danbi/src/App.js
+++ FE/danbi/src/App.js
@@ -10,14 +10,38 @@ import { setTheme } from "./store/Slice/settingSlice.js";
 import styled from 'styled-components';
 import Modal from "./Components/Common/Modal/Modal.jsx";
 import JSconfetti from 'js-confetti';
+import Loading from "./Components/Common/Loading/Loading.jsx";
+import axios from 'axios';
 
 // 뽑기 이벤트 Canvas 생성
+// confetti canvas 생성
 export const Jsconfetti = new JSconfetti();
 
 function App() {
-  // confetti canvas 생성
-  //FIXME : 자동로그인, accesstoken만료시 재발급
   const dispatch = useDispatch();
+  const [loading,setLoading] = useState(false);
+
+  useEffect(()=>{
+    //axios 호출시 인터셉트
+    axios.interceptors.request.use(function (config) {
+      if(config.url.includes('detection')){
+        setLoading(true);
+      }
+      return config
+    }, function (error) {
+      return Promise.reject(error);
+    });
+
+    //axios 호출 종료시 인터셉트
+    axios.interceptors.response.use(function (response) {      
+      setLoading(false);
+      return response;
+    }, function (error) {
+      setLoading(false)
+      return Promise.reject(error);
+    });
+  },[])
+
   useEffect(()=>{
     //DO : 쿠키에 저장된 theme을 불러와 redux에 저장
     const savedTheme = getCookie('theme');
@@ -25,6 +49,7 @@ function App() {
   },[dispatch])
   const themeMode = useSelector((state) => state.setting.theme);
   const theme = themeMode === 'light' ? light : dark;
+
   return (
     <ThemeProvider theme={theme}>
       <AppWrap className="App">
@@ -43,6 +68,7 @@ function App() {
         </BrowserRouter>
         <Modal/>
       </AppWrap>
+      <Loading/>
     </ThemeProvider>
   );
 }
