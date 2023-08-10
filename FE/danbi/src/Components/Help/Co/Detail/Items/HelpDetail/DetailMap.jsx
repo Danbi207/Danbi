927cf602eb81aa898af1a61f3de862fe22dab9d3
한승현 <vkflek1232@naver.com>
2023-08-10 14:39:26 +0900

    hotfix : 상세보기 지도 초기 위치값 수정

--- FE/danbi/src/Components/Help/Co/Detail/Items/HelpDetail/DetailMap.jsx
+++ FE/danbi/src/Components/Help/Co/Detail/Items/HelpDetail/DetailMap.jsx
@@ -1,8 +1,14 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Map,CustomOverlayMap   } from 'react-kakao-maps-sdk';
 import styled from 'styled-components';
 
 const DetailMap = ({ position }) => {
+  const [state, setState] = useState({
+    // 지도의 초기 위치
+    center: { lat: 33.452613, lng: 126.570888 },
+    // 지도 위치 변경시 panto를 이용할지에 대해서 정의
+    isPanto: true,
+  })
   const markers = useMemo(()=>{
     const res = [];
     let key = 0;
@@ -29,9 +35,19 @@ const DetailMap = ({ position }) => {
     return res;
   },[position])
 
+  useEffect(()=>{
+    if(position.meetLatitude){
+      setState({
+        center: { lat: position.meetLatitude, lng: position.meetLongitude },
+        isPanto: true,
+      });
+    }
+  },[position])
+
   return (
     <Map 
-      center={{lat:position.meetLatitude,lng:position.meetLongitude}}
+      center={state.center}
+      isPanto={state.isPanto}
       style={{width:"100%",height:"100%"}}
       level={7}
     >
