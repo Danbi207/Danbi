2ef1556cb1127e0066ade884d2d2c2110b81072b
윤태웅 <omygog@naver.com>
2023-08-10 21:17:50 +0900

    hotfix : Accuse.jsx에 setmode 두 번 import 한 개 주석 처리

--- FE/danbi/src/Components/Common/Modal/Accuse/Accuse.jsx
+++ FE/danbi/src/Components/Common/Modal/Accuse/Accuse.jsx
@@ -1,21 +1,21 @@
-import React,{ useRef,useState }  from 'react'
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
-import {setMode} from '../../../../store/Slice/ModalSlice';
+import { setMode } from '../../../../store/Slice/ModalSlice';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import {authFilePost} from "../../../../Util/apis/api";
-import {setMode} from "../../../../Util/apis/api"
+import { authFilePost } from '../../../../Util/apis/api';
+// import {setMode} from "../../../../Util/apis/api"
 const Accuse = () => {
   const dispatch = useDispatch();
-  const targetMemberId = useSelector(state=>state.modal.targetMemberId);
+  const targetMemberId = useSelector((state) => state.modal.targetMemberId);
   const hiddenFileInputRef = useRef();
   const [imageSrc, setImageSrc] = useState(null);
-  const [content,setContent] = useState("");
-  const [file,setFile] = useState(null);
-  const [accuseType,setAccuseType] = useState("SEXUAL_HARASSMENT");
+  const [content, setContent] = useState('');
+  const [file, setFile] = useState(null);
+  const [accuseType, setAccuseType] = useState('SEXUAL_HARASSMENT');
   const encodeFileToBase64 = (fileBlob) => {
     //DO : 이미지를 Base64로 인코딩 -> 이미지 미리보기기능
-    if(!fileBlob) return;
+    if (!fileBlob) return;
     const reader = new FileReader();
     reader.readAsDataURL(fileBlob);
     setFile(fileBlob);
@@ -27,71 +27,77 @@ const Accuse = () => {
     });
   };
 
-  const submitAccuse = async()=>{
-    if(!file) {
-      alert("사진을 업로드 해주세요");
+  const submitAccuse = async () => {
+    if (!file) {
+      alert('사진을 업로드 해주세요');
       return;
     }
 
     //DO : 신고
-    try{
+    try {
       const formData = new FormData();
-      formData.append("files", file);
+      formData.append('files', file);
       const json = {
         targetMemberId,
         content,
-        accuseType
+        accuseType,
       };
-      json["files"] = file;
-      for (let key in json ) {
+      json['files'] = file;
+      for (let key in json) {
         formData.append(key, json[key]);
       }
-      const url = "/api/v1/accuse";
-      const res = await authFilePost(url,formData);
-      if(res){
+      const url = '/api/v1/accuse';
+      const res = await authFilePost(url, formData);
+      if (res) {
         dispatch(setMode(null));
       }
-    }catch(err){
+    } catch (err) {
       console.log(err.response);
     }
-  }
+  };
 
   return (
     <ModalWrap>
       <AccuseWrap>
         <div>
           <TitleWrap>신고유형</TitleWrap>
-          <select onChange={e=>setAccuseType(e.target.value)}>
+          <select onChange={(e) => setAccuseType(e.target.value)}>
             <option value="SEXUAL_HARASSMENT">성희롱</option>
             <option value="ABUSE">욕설</option>
             <option value="VIOLENCE">폭력</option>
             <option value="ETC">기타</option>
           </select>
         </div>
-        <CloseBtn onClick={()=>dispatch(setMode(""))}>X</CloseBtn>
+        <CloseBtn onClick={() => dispatch(setMode(''))}>X</CloseBtn>
         <TitleWrap>신고내용</TitleWrap>
-        <ContentWrap onChange={(e)=>setContent(e.target.value)}></ContentWrap>
+        <ContentWrap onChange={(e) => setContent(e.target.value)}></ContentWrap>
         <TitleWrap>신고내용</TitleWrap>
         <ImageWrap>
-          {
-            imageSrc ?
-              <img width="100%" height="100%" alt='' src={imageSrc}/>
-              :<div>이미지가 없습니다</div>
-          }
+          {imageSrc ? (
+            <img width="100%" height="100%" alt="" src={imageSrc} />
+          ) : (
+            <div>이미지가 없습니다</div>
+          )}
         </ImageWrap>
-        <input accept="image/*" ref={hiddenFileInputRef} onChange={(e)=>encodeFileToBase64(e.target.files[0])} style={{display:'none'}} type='file'></input>
-        <UploadBtn onClick={()=>hiddenFileInputRef.current.click()}>업로드</UploadBtn>
+        <input
+          accept="image/*"
+          ref={hiddenFileInputRef}
+          onChange={(e) => encodeFileToBase64(e.target.files[0])}
+          style={{ display: 'none' }}
+          type="file"
+        ></input>
+        <UploadBtn onClick={() => hiddenFileInputRef.current.click()}>업로드</UploadBtn>
         <AccuseBtn onClick={submitAccuse}>신고하기</AccuseBtn>
       </AccuseWrap>
     </ModalWrap>
-  )
-}
+  );
+};
 
 const AccuseBtn = styled.button`
   left: 0;
-  bottom : 0;
+  bottom: 0;
   position: absolute;
-  background-color: #E85151;
+  background-color: #e85151;
   height: 3rem;
   margin-left: 10%;
   width: 80%;
@@ -99,77 +105,76 @@ const AccuseBtn = styled.button`
     width: 80%;
   }
   border-radius: 0.5rem;
-`
+`;
 const UploadBtn = styled.button`
   margin-top: 0.5rem;
   margin-left: 10%;
   width: 5rem;
   height: 1.5rem;
   border-radius: 0.5rem;
-  background-color: #6161FF;
-`
+  background-color: #6161ff;
+`;
 
 const ImageWrap = styled.div`
   margin-left: 10%;
   width: 80%;
   height: 8rem;
-  border: 1px solid ${props=>props.theme.colors.titleColor};
+  border: 1px solid ${(props) => props.theme.colors.titleColor};
   border-radius: 0.5rem;
-  background-color: ${props=>props.theme.colors.bgColor};
-  color: ${props=>props.theme.colors.titleColor};
-  
-  &>img{
+  background-color: ${(props) => props.theme.colors.bgColor};
+  color: ${(props) => props.theme.colors.titleColor};
+
+  & > img {
     object-fit: contain;
   }
 
-  &>div{
-    width : 100%;
+  & > div {
+    width: 100%;
     height: 100%;
     text-align: center;
     line-height: 8rem;
   }
-`
+`;
 
 const ContentWrap = styled.textarea`
   margin-left: 10%;
   width: 80%;
   height: 10rem;
   padding: 0.5rem;
-  border: 1px solid ${props=>props.theme.colors.titleColor};
+  border: 1px solid ${(props) => props.theme.colors.titleColor};
   border-radius: 0.5rem;
-  background-color: ${props=>props.theme.colors.bgColor};
-  color: ${props=>props.theme.colors.titleColor};
+  background-color: ${(props) => props.theme.colors.bgColor};
+  color: ${(props) => props.theme.colors.titleColor};
   resize: none;
-`
+`;
 
 const TitleWrap = styled.div`
   font-size: 1.5rem;
   margin: 0.5rem 0;
-`
+`;
 
 const CloseBtn = styled.button`
   position: absolute;
   top: 0;
   right: 0;
-`
+`;
 const AccuseWrap = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
   padding: 1rem 0;
 
-  &>:first-child{
+  & > :first-child {
     display: flex;
     align-items: center;
   }
 
-  & select{
+  & select {
     margin-left: 1rem;
     height: 1.5rem;
     font-size: 1rem;
   }
-
-`
+`;
 const ModalWrap = styled.div`
   position: fixed;
   z-index: 6;
@@ -184,6 +189,6 @@ const ModalWrap = styled.div`
     top: 12.5%;
   }
   padding: 1rem;
-  background-color: ${props=>props.theme.colors.bgColor};
-`
-export default Accuse
+  background-color: ${(props) => props.theme.colors.bgColor};
+`;
+export default Accuse;
