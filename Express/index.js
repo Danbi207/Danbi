377267ac2747143bc0edd9d5271760c08d368105
한승현 <vkflek1232@naver.com>
2023-08-13 11:14:44 +0900

    hotfix : try catch 문으로 감싸기

--- Express/index.js
+++ Express/index.js
@@ -16,74 +16,78 @@ let users = {};
 let socketToRoom = {};
 let chatLog = {};
 const maximum = 2;
-io.on("connection", (socket) => {
-  socket.on("join_room", (data) => {
-    if (users[data.room]) {
-      const length = users[data.room].length;
-      if (length === maximum) {
-        socket.to(socket.id).emit("room_full");
-        return;
+try{
+  io.on("connection", (socket) => {
+    socket.on("join_room", (data) => {
+      if (users[data.room]) {
+        const length = users[data.room].length;
+        if (length === maximum) {
+          socket.to(socket.id).emit("room_full");
+          return;
+        }
+        users[data.room].push({ id: socket.id });
+      } else {
+        users[data.room] = [{ id: socket.id }];
       }
-      users[data.room].push({ id: socket.id });
-    } else {
-      users[data.room] = [{ id: socket.id }];
-    }
-    socketToRoom[socket.id] = data.room;
-
-    socket.join(data.room);
-    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`);
-
-    const usersInThisRoom = users[data.room].filter(
-      (user) => user.id !== socket.id
-    );
-
-    console.log(usersInThisRoom);
-    io.sockets.to(socket.id).emit("all_users", usersInThisRoom);
-  });
-  socket.on("message",async (sdp)=>{
-    console.log("message: " + socket.id);
-    if(!(socketToRoom[socket.id] in chatLog)){
-      chatLog[socketToRoom[socket.id]]=[];
-    }
-    chatLog[socketToRoom[socket.id]].push(sdp);
-    socket.broadcast.emit("message", sdp);
-  })
-  socket.on("offer", (sdp) => {
-    console.log("offer: " + socket.id);
-    socket.broadcast.emit("getOffer", sdp);
-  });
-
-  socket.on("answer", (sdp) => {
-    console.log("answer: " + socket.id);
-    socket.broadcast.emit("getAnswer", sdp);
-  });
-
-  socket.on("candidate", async (candidate) => {
-    console.log("candidate: " + socket.id);
-    socket.broadcast.emit("getCandidate", candidate);
-  });
-
-  socket.on("disconnect", async () => {
-    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
-    const roomID = socketToRoom[socket.id];
-
-    if(socketToRoom[socket.id] in chatLog && chatLog[socketToRoom[socket.id]].length >= 1){
-      await Chat.insertMany(chatLog[socketToRoom[socket.id]]);
-      chatLog[socketToRoom[socket.id]]=[];
-    }
-    let room = users[roomID];
-    if (room) {
-      room = room.filter((user) => user.id !== socket.id);
-      users[roomID] = room;
-      if (room.length === 0) {
-        delete users[roomID];
-        return;
+      socketToRoom[socket.id] = data.room;
+  
+      socket.join(data.room);
+      console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`);
+  
+      const usersInThisRoom = users[data.room].filter(
+        (user) => user.id !== socket.id
+      );
+  
+      console.log(usersInThisRoom);
+      io.sockets.to(socket.id).emit("all_users", usersInThisRoom);
+    });
+    socket.on("message",async (sdp)=>{
+      console.log("message: " + socket.id);
+      if(!(socketToRoom[socket.id] in chatLog)){
+        chatLog[socketToRoom[socket.id]]=[];
       }
-    }
-    socket.broadcast.to(room).emit("user_exit", { id: socket.id });
-    console.log(users);
+      chatLog[socketToRoom[socket.id]].push(sdp);
+      socket.broadcast.emit("message", sdp);
+    })
+    socket.on("offer", (sdp) => {
+      console.log("offer: " + socket.id);
+      socket.broadcast.emit("getOffer", sdp);
+    });
+  
+    socket.on("answer", (sdp) => {
+      console.log("answer: " + socket.id);
+      socket.broadcast.emit("getAnswer", sdp);
+    });
+  
+    socket.on("candidate", async (candidate) => {
+      console.log("candidate: " + socket.id);
+      socket.broadcast.emit("getCandidate", candidate);
+    });
+  
+    socket.on("disconnect", async () => {
+      console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
+      const roomID = socketToRoom[socket.id];
+  
+      if(socketToRoom[socket.id] in chatLog && chatLog[socketToRoom[socket.id]].length >= 1){
+        await Chat.insertMany(chatLog[socketToRoom[socket.id]]);
+        chatLog[socketToRoom[socket.id]]=[];
+      }
+      let room = users[roomID];
+      if (room) {
+        room = room.filter((user) => user.id !== socket.id);
+        users[roomID] = room;
+        if (room.length === 0) {
+          delete users[roomID];
+          return;
+        }
+      }
+      socket.broadcast.to(room).emit("user_exit", { id: socket.id });
+      console.log(users);
+    });
   });
-});
+}catch(err){
+  console.log(err);
+}
 
 server.listen(`${PORT}`, () => {
   console.log(`server running on ${PORT}`);
