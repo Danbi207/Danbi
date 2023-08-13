3f8b7b6d369a18d5caae255d90532fd7983070ac
윤태웅 <omygog@naver.com>
2023-08-13 23:29:48 +0900

    hotfix : 1500ms

--- FE/danbi/src/Components/User/Profile/Preset/PresetDetail.jsx
+++ FE/danbi/src/Components/User/Profile/Preset/PresetDetail.jsx
@@ -14,6 +14,7 @@ const PresetDetail = ({
   sequence,
   setPresetList,
 }) => {
+  const [Recording, setRecording] = useState(false);
   const [value, setValue] = useState(content);
   // 녹음 시작 (리셋하면서 시작)
   const StartRecord = () => {
@@ -107,7 +108,6 @@ const PresetDetail = ({
       SpeechRecognition.startListening({ continuous: true });
     }
   }, [browserSupportsSpeechRecognition]);
-  const [Recording, setRecording] = useState(false);
 
   return (
     <PresetDetailWrap>
