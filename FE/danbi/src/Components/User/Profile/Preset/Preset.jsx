fb3c7628bcebebaecf324daac408bbe5c9dc68da
윤태웅 <omygog@naver.com>
2023-08-14 09:04:53 +0900

    hotfix : build error

--- FE/danbi/src/Components/User/Profile/Preset/Preset.jsx
+++ FE/danbi/src/Components/User/Profile/Preset/Preset.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import PresetItem from './PresetItem.jsx';
