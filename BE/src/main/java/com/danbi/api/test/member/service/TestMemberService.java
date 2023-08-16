d306b0eaf06aa4dbdb854629e2bdd6e3f4717a3c
김윤욱 <hm03048@naver.com>
2023-08-16 09:50:26 +0900

    feat: profile url 수정

--- BE/src/main/java/com/danbi/api/test/member/service/TestMemberService.java
+++ BE/src/main/java/com/danbi/api/test/member/service/TestMemberService.java
@@ -49,7 +49,7 @@ public class TestMemberService {
                 .password(request.getPassword())
                 .role(Role.from(request.getRole()))
                 .gender(Gender.from(request.getGender()))
-                .profileUrl("https://media.hellobot.co/fixedmenu/%E1%84%89%E1%85%B5%E1%84%85%E1%85%A9_%E1%84%8B%E1%85%A1%E1%84%86%E1%85%AE%20%E1%84%8B%E1%85%B5%E1%84%85%E1%85%B3%E1%86%B7%20%E1%84%8C%E1%85%B5%E1%86%BA%E1%84%80%E1%85%B5.png")
+                .profileUrl("http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_110x110.jpg")
                 .build();
 
         memberService.registerMember(member);
