15b584129ae860a63a2678462cdf6325c509a03b
김윤욱 <hm03048@naver.com>
2023-08-17 09:41:48 +0900

    fix: test 계정 로그인 dto 생성자

--- BE/src/main/java/com/danbi/api/test/member/dto/TestMemberLoginRequest.java
+++ BE/src/main/java/com/danbi/api/test/member/dto/TestMemberLoginRequest.java
@@ -1,12 +1,16 @@
 package com.danbi.api.test.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestMemberLoginRequest {
 
     @NotBlank
